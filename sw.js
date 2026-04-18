fba8d3961b70d25fa2243d726cda259156fff05f
FA5DH <ffrrffrr4111@gmail.com>
2026-04-18 22:01:53 +0300

    Update service worker to cache files indefinitely

--- sw.js
+++ sw.js
@@ -1,34 +1,24 @@
-const CACHE_NAME = 'mining-empire-v1';
-const APP_URL = 'https://cdn-consumer.marketjs.com/game/idle-mining-empire/';
+const CACHE_NAME = 'mining-empire-eternal-v1';
+// قائمة الملفات التي سيتم تخزينها في الجوال للأبد
+const urlsToCache = [
+  './',
+  './index.html',
+  './game.css?v=1776229083',
+  './game.js?v=1776229083'
+];
 
-// عند التثبيت: سحب الملفات الأساسية
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll([
-                './index.html',
-                APP_URL
-            ]);
-        })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-// اعتراض الطلبات: إذا كان الملف موجود في الذاكرة سحب منه، وإلا سحبه من الإنترنت وحفظه
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            if (response) return response; // إذا موجود محلياً
-            
-            return fetch(event.request).then((networkResponse) => {
-                // حفظ النسخة الجديدة في الذاكرة للاستخدام القادم
-                return caches.open(CACHE_NAME).then((cache) => {
-                    cache.put(event.request, networkResponse.clone());
-                    return networkResponse;
-                });
-            });
-        }).catch(() => {
-            // إذا فشل الإنترنت وفشل التخزين (حالة طوارئ)
-            return caches.match(APP_URL);
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
